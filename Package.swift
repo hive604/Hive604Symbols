28d6f65e7cfff42d59ec80ee96658ab231ce8c88
Steven Fisher <108106+tewha@users.noreply.github.com>
2026-05-27 13:11:59 -0700

    Fix typo.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Hive6Symbols",
+            name: "HiveSymbols",
             targets: ["HiveSymbols"]
         ),
     ],
